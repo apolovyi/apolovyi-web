f8d630834ef90749bbce37cbc11c84a50bf14cbd
Artem Polovyi <artem.polovyi@gmail.com>
2024-08-14 22:16:25 +0200

    clean up

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,24 +1,16 @@
 import "../styles/globals.css";
 import AppContext from "../components/AppContextFolder/AppContext";
-import { useRef, useState } from "react";
+import {useState} from "react";
 
-function MyApp({ Component, pageProps }) {
-  const timerCookie = useRef(null);
-  const windowSizeTrackerRef = useRef(null);
-  const mousePositionRef = useRef(null);
-  const [sharedState, setSharedState] = useState({
-    userdata: {
-      timerCookieRef: timerCookie,
-      windowSizeTracker: windowSizeTrackerRef,
-      mousePositionTracker: mousePositionRef,
-    },
-    finishedLoading: false,
-  });
-  return (
-    <AppContext.Provider value={{ sharedState, setSharedState }}>
-      <Component {...pageProps} />
-    </AppContext.Provider>
-  );
+function MyApp({Component, pageProps}) {
+    const [sharedState, setSharedState] = useState({
+        finishedLoading: false,
+    });
+    return (
+        <AppContext.Provider value={{sharedState, setSharedState}}>
+            <Component {...pageProps} />
+        </AppContext.Provider>
+    );
 }
 
 export default MyApp;
