ac00e856a46fe1b50865c2155bccb9fd70054b35
Artem Polovyi <artem.polovyi@gmail.com>
2024-08-14 22:12:11 +0200

    clean up

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -2,29 +2,28 @@ import React from "react";
 import GithubIcon from "../Icons/GithubIcon";
 import LinkedinIcon from "../Icons/LinkedinIcon";
 import InstagramIcon from "../Icons/InstagramIcon";
-import YoutubeIcon from "../Icons/YoutubeIcon";
+
 const ClickableIcon = props => {
-  return (
-    <a href={props.href} className="" target={"_blank"} rel="noreferrer">
-      <props.Icon className={"w-5 h-5 text-gray-400 hover:text-AAsecondary fill-current hover:cursor-pointer"} />
-    </a>
-  );
+    return (
+        <a href={props.href} className="" target={"_blank"} rel="noreferrer">
+            <props.Icon className={"w-5 h-5 text-gray-400 hover:text-AAsecondary fill-current hover:cursor-pointer"}/>
+        </a>
+    );
 };
 const IconsData = [
-  { href: "https://github.com/apolovyi", Icon: GithubIcon },
-  { href: "https://www.linkedin.com/in/apolovyi/", Icon: LinkedinIcon },
-  { href: "https://www.instagram.com/artem_polevoi/", Icon: InstagramIcon },
+    {href: "https://github.com/apolovyi", Icon: GithubIcon},
+    {href: "https://www.linkedin.com/in/apolovyi/", Icon: LinkedinIcon},
+    {href: "https://www.instagram.com/artem_polevoi/", Icon: InstagramIcon},
 ];
 
 export default function Footer(props: { hideSocialsInDesktop: boolean }) {
-  return (
-    <div className="bg-AAprimary flex flex-col justify-center items-center py-8 space-y-4">
-      {/* // ? Reach me at */}
-      <div className={`flex flex-row space-x-8 ${props.hideSocialsInDesktop ? "lg:hidden" : ""}`}>
-        {IconsData.map((iconData, index) => {
-          return <ClickableIcon key={index} href={iconData.href} Icon={iconData.Icon} />;
-        })}
-      </div>
-    </div>
-  );
+    return (
+        <div className="bg-AAprimary flex flex-col justify-center items-center py-8 space-y-4">
+            <div className={`flex flex-row space-x-8 ${props.hideSocialsInDesktop ? "lg:hidden" : ""}`}>
+                {IconsData.map((iconData, index) => {
+                    return <ClickableIcon key={index} href={iconData.href} Icon={iconData.Icon}/>;
+                })}
+            </div>
+        </div>
+    );
 }
