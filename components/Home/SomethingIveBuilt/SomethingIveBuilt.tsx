32fe28bf7a807ef21b5f7cecf0d40b0bc4d468ae
Artem Polovyi <artem.polovyi@gmail.com>
2024-08-14 22:51:42 +0200

    blur loading

--- components/Home/SomethingIveBuilt/SomethingIveBuilt.tsx
+++ components/Home/SomethingIveBuilt/SomethingIveBuilt.tsx
@@ -118,6 +118,7 @@ export default function SomethingIveBuilt() {
                                 <Image
                                     src={project.image}
                                     alt={project.title}
+                                    placeholder="blur"
                                     fill
                                     sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
                                     style={{
