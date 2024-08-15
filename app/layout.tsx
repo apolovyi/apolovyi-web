975562e53d4fe730f0f039d69650e09bd6b48017
Artem Polovyi <artem.polovyi@gmail.com>
2024-08-15 17:29:29 +0200

    add fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,42 @@
-import "styles/globals.css";
+import { Arimo, Comfortaa, JetBrains_Mono, Quicksand } from "next/font/google";
 import { AppProvider } from "./shared/AppContext";
+import "styles/globals.css";
+
+const comfortaa = Comfortaa({
+  subsets: ["latin"],
+  variable: "--font-comfortaa",
+  display: "swap",
+});
+
+const quicksand = Quicksand({
+  subsets: ["latin"],
+  variable: "--font-quicksand",
+  display: "swap",
+});
+
+const jetbrainsMono = JetBrains_Mono({
+  subsets: ["latin"],
+  variable: "--font-jetbrains-mono",
+  display: "swap",
+});
+
+const arimo = Arimo({
+  subsets: ["latin"],
+  variable: "--font-arimo",
+  display: "swap",
+});
+
+export const metadata = {
+  title: "Your App Name",
+  description: "Your app description",
+};
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <html lang="en">
+    <html
+      lang="en"
+      className={`${comfortaa.variable} ${quicksand.variable} ${jetbrainsMono.variable} ${arimo.variable}`}
+    >
       <body>
         <AppProvider>{children}</AppProvider>
       </body>
