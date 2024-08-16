d1a9f4feef6b8230f88a2eb0749e50698508b74e
Artem Polovyi <artem.polovyi@gmail.com>
2024-08-16 11:42:35 +0200

    refactor

--- components/const/Projects.tsx
+++ components/const/Projects.tsx
@@ -1,7 +1,7 @@
 export const projects: Project[] = [
   // {
   //     category: "E-commerce Development",
-  //     title: "The Bicester Collection",
+  //     company: "The Bicester Collection",
   //     description: "Led the development of a cutting-edge e-commerce platform for a prestigious luxury brand collective in New York City. Implemented headless CMS architecture for enhanced content flexibility and optimized site performance. Focused on SEO optimization to increase visibility and organic traffic.",
   //     image: "/img/projects/bicester-collection.png",
   //     link: "https://www.thebicestercollection.com",
@@ -10,7 +10,7 @@ export const projects: Project[] = [
   // },
   {
     category: "Enterprise Web Development",
-    title: "Infineon Technologies",
+    company: "Infineon Technologies",
     description:
       "Developed and maintained features on infineon.com to optimize user experience and functionality. Designed and conducted comprehensive unit and integration tests to increase software reliability. Utilized a wide range of technologies to deliver high-quality web solutions.",
     image: "/img/projects/infineon.png",
@@ -20,7 +20,7 @@ export const projects: Project[] = [
   },
   {
     category: "Cloud Migration & Optimization",
-    title: "voestalpine AG",
+    company: "voestalpine AG",
     description:
       "Successfully migrated voestalpine.com to the AWS cloud, significantly enhancing scalability and performance. Implemented advanced features using Java and Spring, while optimizing database operations with PostgreSQL. Leveraged various AWS services to ensure robust and efficient cloud infrastructure.",
     image: "/img/projects/voestalpine.png",
@@ -30,7 +30,7 @@ export const projects: Project[] = [
   },
   {
     category: "Financial Technology",
-    title: "Spreadshirt Payout System",
+    company: "Spreadshirt Payout System",
     description:
       "Developed a high-performance application for payout generation, significantly improving financial operations. Created intuitive user interfaces to enhance user experience and application usability. Implemented robust backend logic to ensure accurate and timely payouts.",
     image: "/img/projects/spreadshirt.png",
@@ -40,7 +40,7 @@ export const projects: Project[] = [
   },
   {
     category: "Car Rental Platform",
-    title: "ABI Audi On Demand",
+    company: "ABI Audi On Demand",
     description:
       "Collaborated in a Scrum team to optimize pricing, voucher management, and communication systems for the ABI Audi On Demand product. Developed and maintained multiple microservices and microfrontends handling loads of up to 200 QPS. Refactored code using Kotlin, increasing maintainability and reducing code lines by 30%.",
     image: "/img/projects/aod.png",
@@ -50,7 +50,7 @@ export const projects: Project[] = [
   },
   {
     category: "Travel & Lifestyle",
-    title: "Turkey Nomad",
+    company: "Turkey Nomad",
     description:
       "Developed a comprehensive platform for digital nomads in Turkey, featuring a travel blog and curated recommendations. This project showcases modern web development techniques and content management integration. The site offers valuable insights and resources for remote workers and travelers exploring Turkey.",
     image: "/img/projects/turkey-nomad.png",
@@ -60,7 +60,7 @@ export const projects: Project[] = [
   },
   {
     category: "Travel Technology",
-    title: "blookery",
+    company: "blookery",
     description:
       "Developed the front-end of an innovative travel website specializing in blind bookings across Europe. Engineered responsive and intuitive user interfaces using React, ensuring seamless functionality across various devices and screen sizes. Implemented advanced JavaScript features to create dynamic and interactive elements, significantly improving user interaction and site navigation.",
     image: "/img/projects/blookery.png",
@@ -70,7 +70,7 @@ export const projects: Project[] = [
   },
   // {
   //     category: "Volunteer Project",
-  //     title: "SmartDorm",
+  //     company: "SmartDorm",
   //     description: "Developed a web application to streamline and automate management processes in the student dormitory Geschwister Scholl. Migrated and consolidated data into a central database to ensure 100% data consistency. Reduced administrative work by 35% through digitalization and automation. Implemented a CI/CD pipeline to optimize the development process.",
   //     image: "/img/projects/blookery.png",
   //     link: "https://smartdorm.schollheim.net",
@@ -79,7 +79,7 @@ export const projects: Project[] = [
   // }
   {
     category: "Car Rental Platform",
-    title: "Billiger Mietwagen",
+    company: "Billiger Mietwagen",
     description:
       "As a student developer, contributed to a leading car rental platform, significantly improving user experience. Implemented A/B tests to optimize product specifications and enhance conversion rates. Conducted thorough testing in each product iteration to ensure high-quality releases. Created internal tools for the customer service team, increasing efficiency by 50%.",
     image: "/img/projects/billiger-mietwagen.png",
@@ -91,7 +91,7 @@ export const projects: Project[] = [
 
 export type Project = {
   category: string;
-  title: string;
+  company: string;
   description: string;
   image: string;
   link: string;
