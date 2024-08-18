52ef50fb6c171f281eefba20ee1babb82c3e33a9
Artem Polovyi <artem.polovyi@gmail.com>
2024-08-18 12:16:36 +0200

    internationalize project

--- i18n-config.ts
+++ i18n-config.ts
@@ -1,6 +1,6 @@
 export const i18n = {
   defaultLocale: "en" as const,
-  locales: ["en", "de", "ua", "ru"] as const,
+  locales: ["en", "de", "ch", "ua", "ru"] as const,
 };
 
 export type Locale = (typeof i18n)["locales"][number];
