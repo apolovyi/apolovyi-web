ce7fb471d534a0dc5de2a64f03eee44ee75f79cd
Artem Polovyi <artem.polovyi@gmail.com>
2024-08-18 20:41:50 +0200

    add robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: "/",
       disallow: "/api/",
     },
-    sitemap: "https://apolovy.me/sitemap.xml",
+    sitemap: "https://apolovyi.me/sitemap.xml",
   };
 }
