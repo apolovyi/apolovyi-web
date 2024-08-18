0ec97d13ec6f4006f1a31a73c818f25127b10eed
Artem Polovyi <artem.polovyi@gmail.com>
2024-08-18 15:26:22 +0200

    logo wip

--- components/header/menu/HexagonLogo.tsx
+++ components/header/menu/HexagonLogo.tsx
@@ -6,13 +6,20 @@ interface LogoProps {
   color?: string;
 }
 
-const HexagonLogo = ({ width = 50, height = 50, color = "#ea5555" }: LogoProps) => (
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" width={width} height={height}>
+const HexagonLogo = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" width="50" height="50">
+    <style>
+      {`
+        .hexagon { fill: #fafafa; stroke: #ea5555; stroke-width: 2; }
+        .text { font-size: 40px; fill: #ea5555; font-weight: 200; }
+      `}
+    </style>
+
     {/* Hexagon */}
-    <polygon className="fill-neutral-50 stroke-accent-coral stroke-2" points="50,5 95,30 95,70 50,95 5,70 5,30" />
+    <polygon className="hexagon" points="50,5 95,30 95,70 50,95 5,70 5,30" />
 
     {/* AP Text */}
-    <text x="50" y="62" textAnchor="middle" className="font fill-accent-coral font-tech text-4xl">
+    <text x="50" y="62" textAnchor="middle" className="text">
       AP
     </text>
   </svg>
