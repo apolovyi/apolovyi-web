b4d77de40160ec6ffe79f5f58e79ada46496972d
Artem Polovyi <artem.polovyi@gmail.com>
2024-08-18 02:09:33 +0200

    localize hero section

---
+++ app/page.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { useAppContext } from "@/components/shared/AppContext";
+import Aos from "aos";
+import "aos/dist/aos.css";
+
+import Header from "@/components/header/Header";
+import HeroSection from "@/components/home/HeroSection";
+import SocialMediaAround from "@/components/home/SocialMediaAround";
+import AboutMe from "@/components/home/AboutMe";
+import MyExperience from "@/components/home/MyExperience";
+import MyProjects from "@/components/home/MyProjects";
+import GetInTouch from "@/components/home/GetInTouch";
+import Footer from "@/components/footer/Footer";
+import ScreenSizeDetector from "@/components/dev/ScreenSizeDetector";
+import { Locale } from "@/i18n-config";
+import Startup from "@/components/header/startup/Startup";
+
+function Home({ params: { lang } }: { params: { lang: Locale } }) {
+  const [showElement, setShowElement] = useState(true);
+  const { sharedState, setSharedState } = useAppContext();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowElement(false);
+      setSharedState((prevState) => ({ ...prevState, finishedLoading: true }));
+    }, 4940);
+
+    return () => clearTimeout(timer);
+  }, [setSharedState]);
+
+  useEffect(() => {
+    Aos.init({ duration: 1000, once: true });
+  }, []);
+
+  const isProd = process.env.NODE_ENV === "production";
+
+  return (
+    <main className="relative min-h-screen w-full snap-mandatory bg-background-primary">
+      {!sharedState.finishedLoading && showElement && <Startup />}
+      <Header finishedLoading={sharedState.finishedLoading} lang={lang} />
+      <HeroSection finishedLoading={sharedState.finishedLoading} lang={lang} />
+      <SocialMediaAround finishedLoading={sharedState.finishedLoading} />
+      {sharedState.finishedLoading && (
+        <>
+          <AboutMe lang={lang} />
+          <MyExperience lang={lang} />
+          <MyProjects lang={lang} />
+          <GetInTouch />
+          <Footer />
+        </>
+      )}
+      {!isProd && <ScreenSizeDetector />}
+    </main>
+  );
+}
+
+export default Home;
