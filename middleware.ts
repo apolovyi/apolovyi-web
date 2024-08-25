d281e2e80b7cb31c25db0c1fd0794508ca7f1b11
Artem Polovyi <artem.polovyi@gmail.com>
2024-08-25 15:06:53 +0200

    clean up

--- middleware.ts
+++ middleware.ts
@@ -61,6 +61,6 @@ export const config = {
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
      */
-    "/((?!api|_next/static|_next/image|favicon.ico).*)",
+    "/((?!api|cv|img|_next/static|_next/image|favicon.ico).*)",
   ],
 };
