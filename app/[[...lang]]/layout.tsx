5b475e0aeb648a85f6e59956e99759e941172bf5
Artem Polovyi <artem.polovyi@gmail.com>
2024-08-26 14:51:17 +0200

    add speed insights

--- app/[[...lang]]/layout.tsx
+++ app/[[...lang]]/layout.tsx
@@ -2,7 +2,7 @@ import { Metadata } from 'next'
 import { Comfortaa, IBM_Plex_Mono, Merriweather, Quicksand } from 'next/font/google'
 import Script from 'next/script'
 
-import { Locale, i18n } from '@/i18n-config'
+import { i18n, Locale } from '@/i18n-config'
 
 import '@/app/globals.css'
 
@@ -12,6 +12,7 @@ import { WebVitals } from '@/components/WebVitals'
 import { AppProvider } from '@/components/shared/AppContext'
 
 import { getDictionary } from '@/lib/dictionary'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 // Fonts
 const comfortaa = Comfortaa({
@@ -93,17 +94,18 @@ const RootLayout = ({ children, params }: { children: React.ReactNode; params: {
 			lang={lang}
 			className={`${comfortaa.variable} ${quicksand.variable} ${ibmPlexMono.variable} ${merriweather.variable}`}
 		>
-			<body>
-				<WebVitals />
-				<LanguageDetector />
-				<AppProvider>{children}</AppProvider>
-				<StructuredData />
-				<Script
-					src="https://app.tinyanalytics.io/pixel/ooUXwijEAaOptnOe"
-					strategy="afterInteractive"
-					defer
-				/>
-			</body>
+		<body>
+		<WebVitals />
+		<LanguageDetector />
+		<AppProvider>{children}</AppProvider>
+		<StructuredData />
+		<SpeedInsights />
+		<Script
+			src="https://app.tinyanalytics.io/pixel/ooUXwijEAaOptnOe"
+			strategy="afterInteractive"
+			defer
+		/>
+		</body>
 		</html>
 	)
 }
